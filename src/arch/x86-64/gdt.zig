4f3d5510ae199d631cfc7b2aa101a771795b683f
Malcolm Parry <me@mparry.dev>
2026-07-30 23:57:14 +0100

    added data selector to gdt

--- src/arch/x86-64/gdt.zig
+++ src/arch/x86-64/gdt.zig
@@ -1,25 +1,50 @@
 const GDT = struct {
-    const Entry = u64;
-
     const Ptr = packed struct {
         size_bytes: u16,
         ptr: u64,
     };
 
-    const nullDescriptor: Entry = 0;
-    const codeDescriptor: Entry = (1 << 43) | (1 << 44) | (1 << 47) | (1 << 53);
-    const descriptors: [2]Entry = .{ nullDescriptor, codeDescriptor };
+    const Desc = u64;
+    const executable_bit: Desc = 1 << 43;
+    const code_or_data_bit: Desc = 1 << 44;
+    const present_bit: Desc = 1 << 47;
+    const long_mode_bit: Desc = 1 << 53;
+    const read_write_bit: Desc = 1 << 41;
+    const dpl3_bits: Desc = 3 << 45;
+
+    const descriptors: [3]Desc = .{
+        // null desc
+        0,
+        // kernel code desc
+        executable_bit | code_or_data_bit | present_bit | long_mode_bit | read_write_bit,
+        // kernel data desc
+        present_bit | code_or_data_bit | read_write_bit,
+    };
 };
 
 pub fn init() void {
     const gdtr: GDT.Ptr = .{
         .ptr = @intFromPtr(&GDT.descriptors),
-        .size_bytes = @sizeOf(GDT.Entry) * GDT.descriptors.len - 1,
+        .size_bytes = @sizeOf(GDT.Desc) * GDT.descriptors.len - 1,
     };
 
     asm volatile (
         \\lgdt (%[addr])
         :
-        : [addr] "rax" (&gdtr),
-    );
+        : [addr] "r" (&gdtr),
+        : .{ .memory = true });
+
+    asm volatile (
+        \\push $0x08
+        \\lea 1f(%%rip), %%rax
+        \\push %%rax
+        \\lretq
+        \\1:
+        \\mov $0x10, %%ax
+        \\mov %%ax, %%ds
+        \\mov %%ax, %%es
+        \\mov %%ax, %%fs
+        \\mov %%ax, %%gs
+        \\mov %%ax, %%ss
+        ::: .{ .rax = true, .memory = true });
 }
